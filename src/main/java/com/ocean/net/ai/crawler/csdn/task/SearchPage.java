ac112486c43a5ad46429a19e271e095c8539f979
ocean <ocean@DESKTOP-47HN69M>
2018-02-10 17:41:21 +0800

    add spring

--- src/main/java/com/ocean/net/ai/crawler/csdn/task/SearchPage.java
+++ src/main/java/com/ocean/net/ai/crawler/csdn/task/SearchPage.java
@@ -8,7 +8,7 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class SearchPage {
-	private void search(String keyword) throws Exception {
+	private void searchOkhttp(String keyword) throws Exception {
 
 		// http://so.csdn.net/so/search/s.do?q=%E7%BD%91%E9%A1%B5%E7%88%AC%E8%99%AB+%E6%95%B0%E6%8D%AE%E5%BA%93&t=&o=&s=&l=
 		// http://so.csdn.net/so/search/s.do?q=%E7%BD%91%E9%A1%B5%E7%88%AC%E8%99%AB%20%E6%95%B0%E6%8D%AE%E5%BA%93&t=
@@ -24,5 +24,9 @@ public class SearchPage {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private void searchHtmlUnit() {
+
 	}
 }
---
+++ src/main/java/com/ocean/net/ai/crawler/dao/TestDao.java
@@ -0,0 +1,9 @@
+package com.ocean.net.ai.crawler.dao;
+
+import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
+
+public class TestDao extends HibernateDaoSupport {
+
+	void save() {
+	}
+}
