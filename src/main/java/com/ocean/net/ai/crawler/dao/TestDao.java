1a69c00486dda54eab4efedfa3c35e966a37610e
ocean <ocean@DESKTOP-47HN69M>
2018-04-09 15:21:27 +0800

    test

--- src/main/java/com/ocean/net/ai/crawler/dao/TestDao.java
+++
@@ -1,9 +0,0 @@
-package com.ocean.net.ai.crawler.dao;
-
-import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
-
-public class TestDao extends HibernateDaoSupport {
-
-	void save() {
-	}
-}
