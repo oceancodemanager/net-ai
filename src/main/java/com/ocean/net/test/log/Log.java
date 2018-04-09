1b7cb1a89dab8169eaa27142e910fc794104d2b9
ocean <ocean@DESKTOP-47HN69M>
2018-04-09 18:01:03 +0800

    logback

---
+++ src/main/java/com/ocean/net/test/log/Log.java
@@ -0,0 +1,12 @@
+package com.ocean.net.test.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Log {
+	private static final Logger logger = LoggerFactory.getLogger(Log.class);
+
+	public static void main(String[] args) {
+		logger.debug("111111111111111");
+	}
+}
